14096ff1c955f7e37a8e023bad4efba4567f059f
Igor Bukur <tuturox91@gmail.com>
2022-11-10 15:46:06 +0200

    initial setting fix

--- src/main/resources/init.db.sql
+++ src/main/resources/init.db.sql
@@ -1,6 +1,6 @@
 CREATE SCHEMA `taxiservicedb` DEFAULT CHARACTER SET utf8 ;
 CREATE TABLE `taxiservicedb`.`manufacturers` (
-    `id` BIGINT(1000) NOT NULL AUTO_INCREMENT,
+    `id` BIGINT(255) NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(50) NOT NULL,
     `country` VARCHAR(50) NOT NULL,
     `is_deleted` TINYINT NULL DEFAULT 0,
